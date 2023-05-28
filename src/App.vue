4828697e46a6554aec38a7f861d6dc7d734afd26
Joao Victor Cachola <joao@venue.live>
2023-05-28 10:38:34 -0300

    Add Header to App

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Header from "./components/Header";
+</script>
 
 <template>
   <div class="w-full min-h-screen">
+    <Header />
     <router-view />
   </div>
 </template>
