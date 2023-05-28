1247be1e82eee715e8d7ae1017dbbbb806ba16b5
Joao Victor Cachola <joao@venue.live>
2023-05-28 10:53:42 -0300

    Create Card Component

---
+++ src/components/Card/Card.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps({
+  isLoading: {
+    type: Boolean,
+    default: false,
+  },
+  classNames: {
+    type: String,
+    default: "",
+  },
+});
+</script>
+
+<template>
+  <div
+    class="flex justify-center items-center p-6 rounded-md shadow-lg"
+    :class="`${isLoading && 'bg-gray-200 animate-pulse'} ${classNames}`"
+  >
+    <slot v-if="!isLoading"></slot>
+  </div>
+</template>
