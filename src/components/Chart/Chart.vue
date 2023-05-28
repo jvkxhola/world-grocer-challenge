5afe0f0592ca0f769c75dc24dd64fd5423d429a3
Joao Victor Cachola <joao@venue.live>
2023-05-28 14:45:35 -0300

    Create Chart Component

---
+++ src/components/Chart/Chart.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "vue-chartjs";
+
+type DataSet = {
+  label: string;
+  backgroundColor: string;
+  data: Array<number>;
+};
+
+defineProps({
+  labels: {
+    type: Array<String>,
+    required: true,
+  },
+  datasets: {
+    type: Array<DataSet>,
+    default: [],
+  },
+});
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+</script>
+
+<template>
+  <Line
+    :data="{ labels, datasets }"
+    :options="{
+      responsive: true,
+      maintainAspectRatio: false,
+    }"
+  />
+</template>
