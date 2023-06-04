689142fd5fd35f7518e7dd87869a7bbb6cac8017
Joao Victor Cachola <jvkxhola@gmail.com>
2023-06-04 11:58:52 -0300

    Update Date Picker so I do not need local refs

--- src/components/DatePicker/DatePicker.vue
+++ src/components/DatePicker/DatePicker.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
 import VueDatePicker from "@vuepic/vue-datepicker";
 import "@vuepic/vue-datepicker/dist/main.css";
-import { watch, ref } from "vue";
 
-const props = defineProps({
+defineProps({
   dates: {
     type: Array<Date>,
     required: true,
@@ -18,28 +17,16 @@ const props = defineProps({
   },
 });
 
-const dateRange = ref<Array<Date>>(props.dates);
-
 const emit = defineEmits<{
   (e: "update-model-value", dates: Array<Date>): void;
 }>();
-
-watch(dateRange, (dateRange: Array<Date>) => {
-  emit("update-model-value", dateRange);
-});
-
-watch(
-  () => props.dates,
-  (dates: Array<Date>) => {
-    dateRange.value = dates;
-  }
-);
 </script>
 
 <template>
   <div class="w-fit">
     <VueDatePicker
-      v-model="dateRange"
+      :model-value="dates"
+      @update:model-value="(dates) => emit('update-model-value', dates)"
       :min-date="minDate"
       :max-date="maxDate"
       :enable-time-picker="false"
